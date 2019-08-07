5446b8503a1265d6707f98b0dfef84822f040504
Ghassen Khalil Ati <ghassen1khalil@gmail.com>
2019-08-07 16:56:53 +0200

    cleaning POMs dependencies

--- social-carpooling-backend/src/main/java/tn/social/carpooling/Application.java
+++ social-carpooling-backend/src/main/java/tn/social/carpooling/Application.java
@@ -1,5 +1,6 @@
 package tn.social.carpooling;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
